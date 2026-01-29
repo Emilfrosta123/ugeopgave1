5032b9e5dbaa59de2c616a1d19273695c4f6a2eb
Emilfrosta123 <emil.ahmadi@gmail.com>
2026-01-29 13:18:26 +0100

    fjernet */ (kommentarer)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,135 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+Opgave 2
+void main(){
+    int food = 450;
+    int drinks= 120;
+    int tip = 50;
+    int persons = 4;
+    int totalSum = food+drinks+tip;
+    int totalPerPerson = totalSum/persons;
+    System.out.println(totalPerPerson);
 }
+
+Opgave 3
+void main (){
+    int price=300;
+    double discount = 20;
+    double discountedPrice = price-(price*discount/100);
+    double tax = 25;
+    double total= discountedPrice+(discountedPrice*tax/100);
+    System.out.println(price);
+    System.out.println(discountedPrice);
+    System.out.println(total);
+}
+
+Opgave 4
+void main(){
+    int guests= 15;
+    int cupcakes = 40;
+    int cupcakesPerPerson= cupcakes/guests;
+    int leftover = cupcakes%guests;
+    System.out.println("Each guest gets "+cupcakesPerPerson);
+    System.out.println(leftover+" cupcakes left over");
+}
+
+Opgave 5
+void main(){
+    String name = "Anna";
+    int age = 25;
+    System.out.println("Hello, "+ name+"!"+" You are "+age+" years old.");
+}
+
+
+Opgave 6
+void main (){
+    int score = 100;
+    System.out.println(score);
+    score+=50;
+    System.out.println(score);
+    score=score+25;
+    System.out.println(score);
+}
+Opgave 7
+
+void main (){
+    int health = 100;
+    health=health-15;
+    System.out.println(health);;
+    health=health-20;
+    System.out.println(health);
+}
+Opgave 8
+
+void main(){
+    double price = 50.00;
+    System.out.println(price);
+    price*=2;
+    System.out.println(price);
+}
+Opgave 9
+
+void main (){
+    int counter = 10;
+    System.out.println(counter);
+    counter++;
+    System.out.println(counter);
+    counter--;
+    System.out.println(counter);
+}
+Opgave 10
+
+void main(){
+    int number = 10;
+    number+=5;
+    System.out.println(number);
+    number-=3;
+    System.out.println(number);
+    number++;
+    System.out.println(number);
+    number--;
+    System.out.println(number);
+}
+Opgave 11
+1: String item
+2: double price
+3: boolean isLeft
+4. int itemLeft
+5: double discount
+6: String category
+
+Opgave 12
+1: String name
+2: int points
+3: int pointsFromAssignments
+4: double grade
+5: boolean hasPassed
+
+Opgave 13
+1: String name
+2: int room
+3: int nightsAtHotel
+4: double cost
+5: boolean breakfastIncluded
+6: double totalCost
+
+Opgave 14
+int steps
+int stepsGoal
+double stepsKm
+int caloriesBurned
+boolean goalCompleted
+
+Opgave 15
+1: String bookTitle
+2: String authorName
+3: int isbnNumber
+4: int totalPages
+5: boolean canLoan
+6: String loaner
+7: String returnBook
+8: double taxNotReturned
+9: boolean isBestSeller
+10: double rating
+ */
+
